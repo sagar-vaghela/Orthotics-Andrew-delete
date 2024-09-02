56ad8f45ad4cbd169f91d17e256a78e2b00bc0a9
Mac <mac@Macs-MacBook-Pro-3.local>
2024-09-02 10:17:04 +0530

    initialize project

---
+++ src/lib/settings/apply-default-settings.js
@@ -0,0 +1,13 @@
+import { config } from '@/config';
+
+export function applyDefaultSettings(settings) {
+  return {
+    colorScheme: config.site.colorScheme,
+    primaryColor: config.site.primaryColor,
+    direction: 'ltr',
+    navColor: 'evident',
+    layout: 'vertical',
+    language: config.site.language,
+    ...settings,
+  };
+}
